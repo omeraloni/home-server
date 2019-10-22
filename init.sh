309d471b19359c2ab56a9b9cf9198dab24512658
Omer Aloni <omeraloni@gmail.com>
2019-10-22 23:28:12 +0300

    Added radarr

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
-mkdir shared
-mkdir portainer
-mkdir organizr
-mkdir transmission
+mkdir -p ~/docker/shared
+mkdir -p ~/docker/portainer
+mkdir -p ~/docker/organizr
+mkdir -p ~/docker/transmission
+mkdir -p ~/docker/radarr
+mkdir -p ~/downloads
+mkdir -p ~/watch
+mkdir -p ~/media/movies
+mkdir -p ~/media/tvshows
 
-docker-compose -f docker-compose.yml up -d
+#docker-compose -f docker-compose.yml up -d
