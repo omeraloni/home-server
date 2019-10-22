492fda72f32359ef08d183f8cb0e637c845d398e
Omer Aloni <omeraloni@gmail.com>
2019-10-22 23:54:32 +0300

    Added sonarr

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
+mkdir -p ~/downloads
+mkdir -p ~/watch
+mkdir -p ~/media/movies
+mkdir -p ~/media/tvshows
+
 mkdir -p ~/docker/shared
 mkdir -p ~/docker/portainer
 mkdir -p ~/docker/organizr
 mkdir -p ~/docker/transmission
 mkdir -p ~/docker/radarr
-mkdir -p ~/downloads
-mkdir -p ~/watch
-mkdir -p ~/media/movies
-mkdir -p ~/media/tvshows
+mkdir -p ~/docker/sonarr
 
 #docker-compose -f docker-compose.yml up -d
